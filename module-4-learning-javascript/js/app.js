d0e204d898b92addeb3e65338083d401ee2b129f
arif <amuhammadbdimshealth@gmail.com>
2016-11-07 19:22:49 +0600

    module4-assignment-solution

---
+++ module-4-learning-javascript/js/app.js
@@ -0,0 +1,11 @@
+window.yaakovGreeter.sayHello();
+window.johnGreeter.sayHi();
+
+// Immediately Invoked Function Expression
+// IIFE
+(function (name) {
+  console.log("Hello " + name);
+})("Coursera!");
+
+
+(function(){})()
