575240575128901c6c47621c97db47f9da857dbe
arif <amuhammadbdimshealth@gmail.com>
2016-11-13 15:04:06 +0600

    lecture60-dynamically load home view content

--- module5-coding-restuarant-site/js/script.js
+++ module5-coding-restuarant-site/js/script.js
@@ -1,3 +1,4 @@
+
 $(function(){
 	$(".navbar-toggle").on("blur",function(){
 		$("#collapsable-nav").collapse("hide");
@@ -6,3 +7,27 @@ $(function(){
 	console.log("ARif");
 });
 
+(function(global){
+	var dc = {};
+
+	var homeHtml = "../snippets/home-snippet.html"
+	// Convenience function for inserting innerHTML for 'select'
+	var insertHtml = function(selector,html){
+		$(selector).html(html);
+	};
+	// Convenience function for inserting innerHTML for 'select'
+	var showLoadingGif = function(selector){
+		var loadingGif = "<div class='text-center'><img src='images/ajax-loader.gif'></div>"
+		insertHtml(selector,loadingGif);
+	};
+	$(function(){
+		// On first load, show home view
+		showLoadingGif("#main-content");
+		$ajaxUtils.sendGetRequest(homeHtml,responseHandler,false);
+		function responseHandler(response){
+			insertHtml("#main-content",response)
+		}
+	});//DOM loaded
+
+	global.$dc = dc;
+})(window);//IIFE
